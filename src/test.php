a095323b94feadeef9a0bda9cb58521b4c06d23d
rinchoku <gazar.24865@gmail.com>
2023-01-29 10:06:09 +0900

    feat: connet mysql to web

--- src/test.php
+++ src/test.php
@@ -1,4 +1,11 @@
 <?php
 
-print("Hellow World");
+print("Hello World");
 
+$db = new PDO('mysql:dbname=mysql_db;host=mysql', 'user', 'user_pass');
+
+$sql = 'SELECT version();';
+$contact = $db->prepare($sql);
+$contact->execute();
+$result = $contact->fetchAll(PDO::FETCH_ASSOC);
+var_dump($result);
